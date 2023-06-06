8ba88f3b58fac98bfbd657bf0365f22509de1a27
slowisfast <slowisfast2030@163.com>
2023-06-06 17:48:08 +0800

    less is more

--- contracts/UniswapV2Router02.sol
+++ contracts/UniswapV2Router02.sol
@@ -3,6 +3,9 @@ pragma solidity =0.6.6;
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 
+// 添加了这一行
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+
 import './interfaces/IUniswapV2Router02.sol';
 import './libraries/UniswapV2Library.sol';
 import './libraries/SafeMath.sol';
